b23280cf3d0ef98051d5c8623a995305e7e47fc9
Stinkin <stinkinevil@hotmail.com>
2023-01-24 18:30:42 -0300

    Segundo Commit, cambio de diseño de botones

--- js/script.js
+++ js/script.js
@@ -1,45 +1,13 @@
-function clickQco() {
+
+function clickParam(param) {
   const cols = document.querySelectorAll(".parte");
-  const cols2 = document.querySelectorAll(".a");
+  const cols2 = document.querySelectorAll(param);
 
   cols.forEach((col) => {
     col.classList.add("none");
   });
-
+  
   cols2.forEach((col2) => {
     col2.classList.remove("none");
-  });
-}
-
-function clickBar() {
-  const cols = document.querySelectorAll(".parte");
-  const cols2 = document.querySelectorAll(".b");
-
-  cols.forEach((col) => {
-    col.classList.add("none");
-  });
-
-  cols2.forEach((col2) => {
-    col2.classList.remove("none");
-  });
-}
-
-function clickSham() {
-  const cols = document.querySelectorAll(".parte");
-  const cols2 = document.querySelectorAll(".c");
-
-  cols.forEach((col) => {
-    col.classList.add("none");
-  });
-
-  cols2.forEach((col2) => {
-    col2.classList.remove("none");
-  });
-}
-function clickAll() {
-  const cols = document.querySelectorAll(".parte");
-
-  cols.forEach((col) => {
-    col.classList.remove("none");
   });
 }
